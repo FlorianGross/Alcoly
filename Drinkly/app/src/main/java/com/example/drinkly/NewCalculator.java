a802a4f0a913aed617e98f0140c3238e3489fc0d
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2020-11-17 01:53:33 +0100

    Worked with nested RecyclerView

--- Drinkly/app/src/main/java/com/example/drinkly/NewCalculator.java
+++ Drinkly/app/src/main/java/com/example/drinkly/NewCalculator.java
@@ -2,6 +2,7 @@ package com.example.drinkly;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.view.View;
 
 import com.example.drinkly.NonMain.DatabaseHelper;
 import com.example.drinkly.NonMain.Getränke;
+import com.example.drinkly.NonMain.GroupAdapter;
 import com.example.drinkly.NonMain.myAdapter;
 
 import java.util.ArrayList;
@@ -19,13 +21,15 @@ public class NewCalculator extends AppCompatActivity {
     private DatabaseHelper databaseHelper;
     private ArrayList<Getränke> arrayList;
     private myAdapter adapter;
+    private GroupAdapter groupAdapter;
+    private ArrayList<String> arrayListString;
     private myAdapter.RecyclerViewClickListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_calculator);
-        databaseHelper = new DatabaseHelper(getApplicationContext());
+        /*databaseHelper = new DatabaseHelper(getApplicationContext());
         arrayList = databaseHelper.getAllGetraenke();
 
         RecyclerView mRecyclerView = findViewById(R.id.mRecyclerView);
@@ -36,6 +40,12 @@ public class NewCalculator extends AppCompatActivity {
         adapter = new myAdapter(this, arrayList, listener);
         mRecyclerView.setAdapter(adapter);
         mRecyclerView.setHasFixedSize(false);
+*/
+        RecyclerView newRecyclerView = findViewById(R.id.mRecyclerView);
+        newRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        groupAdapter = new GroupAdapter(this, arrayListString);
+        newRecyclerView.setAdapter(groupAdapter);
+        newRecyclerView.setHasFixedSize(false);
 
 
     }
--- Drinkly/app/src/main/java/com/example/drinkly/NonMain/GroupAdapter.java
+++ Drinkly/app/src/main/java/com/example/drinkly/NonMain/GroupAdapter.java
@@ -1,24 +1,78 @@
 package com.example.drinkly.NonMain;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class GroupAdapter extends RecyclerView.Adapter<myAdapter.ViewHolder> {
+import com.example.drinkly.Details;
+import com.example.drinkly.R;
+
+import java.util.ArrayList;
+
+public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.myViewHolder> {
+    private Context context;
+    ArrayList<String> arrayListGroup;
+    private myAdapter.RecyclerViewClickListener listener;
+
+    public GroupAdapter(Context context, ArrayList<String> arrayListGroup) {
+        this.context = context;
+        this.arrayListGroup = arrayListGroup;
+    }
+
     @NonNull
     @Override
-    public myAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_group, parent, false);
+        return new GroupAdapter.myViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull myAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        holder.tv.setText(arrayListGroup.get(position));
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        ArrayList<Getränke> getränkeList = databaseHelper.getAllGetraenke();
+        int numberOfColumns = 3;
 
+        setOnCLickListener();
+        holder.rv.setLayoutManager(new GridLayoutManager(context, numberOfColumns));
+        myAdapter adapter = new myAdapter(context, getränkeList, listener);
+        holder.rv.setAdapter(adapter);
+        holder.rv.setHasFixedSize(false);
+
+
+    }
+    private void setOnCLickListener() {
+        listener = new myAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(context, Details.class);
+                intent.putExtra("intPosition", position);
+                context.startActivity(intent);
+            }
+        };
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayListGroup.size();
+    }
+
+    public class myViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+        RecyclerView rv;
+        public myViewHolder(View view) {
+            super(view);
+            tv.findViewById(R.id.DateView);
+            rv.findViewById(R.id.FirstRecyclerView);
+        }
     }
 }
+
+
