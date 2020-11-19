3bd355d4cabe4230f5bb882a798e083b120d6129
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2020-11-20 00:38:04 +0100

    Started with the Statistics Page

---
+++ Drinkly/app/src/main/java/com/example/drinkly/hoherWert.java
@@ -0,0 +1,34 @@
+package com.example.drinkly;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class hoherWert extends Fragment {
+
+
+    public hoherWert() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hoher_wert, container, false);
+    }
+}
---
+++ Drinkly/app/src/main/java/com/example/drinkly/mittlererWert.java
@@ -0,0 +1,34 @@
+package com.example.drinkly;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class mittlererWert extends Fragment {
+
+
+    public mittlererWert() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mittlerer_wert, container, false);
+    }
+}
