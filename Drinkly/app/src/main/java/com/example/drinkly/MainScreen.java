bc743e0ecffc8e62101861530569a953ef9c3bdd
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2020-11-23 02:29:35 +0100

    Fixed the Buttons on top of the Main Screen

--- Drinkly/app/src/main/java/com/example/drinkly/MainScreen.java
+++ Drinkly/app/src/main/java/com/example/drinkly/MainScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,7 @@ import java.util.ArrayList;
 public class MainScreen extends AppCompatActivity {
     private ImageView mainButton;
     private TextView textView;
-    private Button leftB, centerB, rightB;
+    private ImageView leftB, centerB, rightB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,7 @@ public class MainScreen extends AppCompatActivity {
         centerB = findViewById(R.id.CenterButton);
         rightB = findViewById(R.id.RightButton);
         textView = findViewById(R.id.promilleErgebnis);
+
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
         boolean firstStart = prefs.getBoolean("firstStart", true);
@@ -72,7 +74,7 @@ public class MainScreen extends AppCompatActivity {
                             NewCalculator calculate = new NewCalculator();
                             DecimalFormat f = new DecimalFormat();
                             f.setMaximumFractionDigits(2);
-                            String promilleString = f.format(calculate.getNormalResultValue(getApplicationContext()) );
+                            String promilleString = f.format(calculate.getNormalResultValue(getApplicationContext()));
                             textView.setText(promilleString + " \u2030");
                         }
                     });
