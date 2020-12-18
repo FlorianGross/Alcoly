595aa3a56a640f1bc1f4cd681f80507e2cc63aed
Florian Groß <54940184+FlorianGross@users.noreply.github.com>
2020-12-18 01:25:09 +0100

    Settings

--- Drinkly/app/src/main/java/com/fmgross/alcoly/fragments/settingsFragment.java
+++ Drinkly/app/src/main/java/com/fmgross/alcoly/fragments/settingsFragment.java
@@ -1,6 +1,10 @@
 package com.fmgross.alcoly.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,20 +37,44 @@ public class settingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
 
-         link = root.findViewById(R.id.datenschutzButton);
-         datenschutz = root.findViewById(R.id.);
-         male = root.findViewById(R.id.);
-         female = root.findViewById(R.id.);
-         age = root.findViewById(R.id.);
-         weight = root.findViewById(R.id.);
-         night = root.findViewById(R.id.);
-         audio = root.findViewById(R.id.);
-         scanOnStart = root.findViewById(R.id.);
+        link = root.findViewById(R.id.formelnButton);
+        datenschutz = root.findViewById(R.id.datenschutzButton);
+        male = root.findViewById(R.id.maleButton);
+        female = root.findViewById(R.id.femaleButton);
+        age = root.findViewById(R.id.AlterInput);
+        weight = root.findViewById(R.id.GewichtInput);
+        night = root.findViewById(R.id.darkmodeSwitch);
+        audio = root.findViewById(R.id.audioSwitch);
+        scanOnStart = root.findViewById(R.id.startWithScanSwitch);
 
 
+        generateSettings();
 
+        datenschutz.setOnClickListener(v -> {
+            Uri uriUrl = Uri.parse("https://fmgross.de/datenschutzerklaerung-von-com-fmgross-alcoly/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+            startActivity(launchBrowser);
+        });
+        link.setOnClickListener(v -> {
+            Uri uriUrl = Uri.parse("https://fmgross.de/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+            startActivity(launchBrowser);
+        });
 
         return root;
 
+    }
+
+    private void generateSettings() {
+        SharedPreferences settings = getContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        if (settings.getString("gender", "Male").equals("Male")) {
+            male.setBackgroundColor(Color.rgb(255, 205, 25));
+            female.setBackgroundColor(Color.rgb(34, 34, 34));
+        } else {
+            male.setBackgroundColor(Color.rgb(34, 34, 34));
+            female.setBackgroundColor(Color.rgb(255, 205, 25));
+        }
+        age.setText(settings.getInt("age", 20));
+        weight.setText(settings.getInt("weight", 80));
     }
 }
